88a139c29ebc7b1770cd3937a884fcd827db45c1
gabbytaz <gabrielaeras@hotmail.com>
2025-07-04 18:42:50 -0500

    formulario reactivo

--- src/app/formulario-reactivo/formulario-reactivo.ts
+++ src/app/formulario-reactivo/formulario-reactivo.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-reactivo.html',
   styleUrl: './formulario-reactivo.css'
 })
 export class FormularioReactivo {
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
+  }
 
 }
