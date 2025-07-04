44f2c41cf2ee6266133520938e51622d5cf3044a
gabbytaz <gabrielaeras@hotmail.com>
2025-07-04 17:24:45 -0500

    proyecto formularios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { FormularioNormal } from './formulario-normal/formulario-normal';
+import { FormularioReactivo } from './formulario-reactivo/formulario-reactivo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path:"",
+    component:FormularioNormal,
+    title:'Form Normal'
+},
+{
+    path:"reactivo",
+    component:FormularioReactivo,
+    title:'Form Reactivo'
+},
+
+];
