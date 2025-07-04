5af1a51c7b4c6f8d7fb3939443794fb31fb91a95
gabbytaz <gabrielaeras@hotmail.com>
2025-07-04 17:59:39 -0500

    formulario normal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLinkActive,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLinkActive, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/formulario-normal/formulario-normal.ts
+++ src/app/formulario-normal/formulario-normal.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-normal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario-normal.html',
   styleUrl: './formulario-normal.css'
 })
 export class FormularioNormal {
+  name: string = '';
+  names: string[] = [];
 
+  enviar() {
+    if (this.name.trim() != "") {
+      this.names.push(this.name.trim());
+      this.name = '';
+    }
+  }
 }
